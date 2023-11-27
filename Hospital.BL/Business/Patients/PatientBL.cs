5711f084650196b34004626e096edc6a173d1156
Loique <Loique-Darios.TAKAM-TALLA@ecole-it.com>
2023-11-27 07:09:21 +0100

    DevTools used as package to the project

--- Hospital.BL/Business/Patients/PatientBL.cs
+++ Hospital.BL/Business/Patients/PatientBL.cs
@@ -1,9 +1,16 @@
-﻿
+﻿//adjust according to the actual devtools structure
+using DevTools.ADO.Repository
 using Hospital.BL.Business;
 
 namespace Hospital.BL.Business.Patient
 {
     public class PatientBL :IPatientBL
     {
+        private readonly IContactRepository _contactRepository;
+
+        public PatientBL(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
     }
 }
